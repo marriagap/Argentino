38838db927e3d1f76252443eb8af5212a95589ce
Mick Arriaga <marriagap@upao.edu.pe>
2013-04-22 01:13:06 -0400

    Subida de proyecto Argentino
    Proyecto de Ventas Argentino

---
+++ Ventas_Milton/Ventas_Milton/Entidades/csAlmacen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ventas_Milton.Datos
+{
+    class csAlmacen
+    {
+        private int _idAlmacen;
+        private int _capMaxLotes;
+
+        public csAlmacen(int idAlmacen, int capMAxXLotes)
+        {
+            this.IdAlmacen = idAlmacen;
+            this.CapMaxLotes = capMAxXLotes;
+        }
+
+        public int IdAlmacen
+        {
+            get { return _idAlmacen; }
+            set { _idAlmacen = value; }
+        }
+
+        public int CapMaxLotes
+        {
+            get { return _capMaxLotes; }
+            set { _capMaxLotes = value; }
+        }
+    }
+}
