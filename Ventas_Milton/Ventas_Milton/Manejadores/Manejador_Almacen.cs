15c646836c94ae59f1ffa6e005cc95e6f343e1e3
MickArriaga <marriagap@upao.edu.pe>
2013-04-25 16:29:47 -0500

    Creación de Métodos Generales en Manejadores

--- Ventas_Milton/Ventas_Milton/Manejadores/Manejador_Almacen.cs
+++ Ventas_Milton/Ventas_Milton/Manejadores/Manejador_Almacen.cs
@@ -2,10 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using Ventas_Milton.Datos;
 
 namespace Ventas_Milton.Manejadores
 {
     class Manejador_Almacen
     {
+        static dAlmacen dalmacen = new dAlmacen();
+
+        /********************************Consultas****************************/
+        public DataTable BuscarAlmacen(string parametro,string valor) {
+            
+            int opcion;
+            DataTable dt = new DataTable();
+
+            if(parametro == "Capacidad")
+            {
+                opcion = 1;
+            }
+            else
+            {
+                if(parametro == "Codigo")
+                {
+                    opcion = 2;
+                }
+                else
+                {
+                    opcion = 3;
+                }
+            }
+
+            switch (opcion)
+            {
+                case 1: dt = dalmacen.ConsultarXCapAlmacen(int.Parse(valor));
+                    break;
+                case 2: dt = dalmacen.ConsultarAlmacenXCodigo(int.Parse(valor));
+                    break;
+                case 3: dt = dalmacen.ConsultarTodos();
+                    break;
+                default: dt = dalmacen.ConsultarTodos();
+                    break;
+            }
+
+            return dt;
+        }
+
+        /***********************ACTUALIZACIONES Y ELIMINACIONES***************/
+        public void ActualizarRegistros_Almacen(string tipoAct, string valor, int cod) 
+        {
+            int opcion;
+            if (tipoAct == "Modificar")
+            {
+                opcion = 1;
+            }
+            else
+            {
+                opcion = 2;
+            }
+
+            switch (opcion) 
+            {
+                case 1: dalmacen.ModificarCapacidad(cod, int.Parse(valor));                        
+                    break;
+                case 2: dalmacen.EliminarAlmacen(cod, int.Parse(valor));
+                    break;
+            }
+        }
     }
 }
--- Ventas_Milton/Ventas_Milton/Manejadores/Manejador_Factores.cs
+++ Ventas_Milton/Ventas_Milton/Manejadores/Manejador_Factores.cs
@@ -2,10 +2,105 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using Ventas_Milton.Datos;
 
 namespace Ventas_Milton.Manejadores
 {
     class Manejador_Factores
     {
+        dFactores dfactores = new dFactores();
+        /***********************************CONSULTAS*****************************/
+        public DataTable BuscarFactor(string parametro, string valor) 
+        {
+            DataTable dt = new DataTable();
+            int opcion;
+            if (parametro == "Codigo")
+            {
+                opcion = 1;
+            }
+            else 
+            {
+                if (parametro == "Tabla")
+                {
+                    opcion = 2;
+                }
+                else
+                {
+                    if (parametro == "Concepto")
+                    {
+                        opcion = 3;
+                    }
+                    else
+                    {
+                        if (parametro == "Monto")
+                        {
+                            opcion = 4;
+                        }
+                        else
+                        { 
+                            opcion = 5;
+                        }
+                    }
+                }
+            }
+
+            switch (opcion) 
+            {
+                case 1: dt = dfactores.ConsultarXCodigo(int.Parse(valor));
+                    break;
+                case 2: dt = dfactores.ConsultarXTabla(valor);
+                    break;
+                case 3: dt = dfactores.ConsultarXConcepto(valor);
+                    break;
+                case 4: dt = dfactores.ConsultarXMonto(valor);
+                    break;
+                case 5: dt = dfactores.ConsultaTodos();
+                    break;
+                default: dt = dfactores.ConsultaTodos();
+                    break;
+            }
+            return dt;
+        }
+
+        /************************ACTUALIZACIONES Y ELIMINACIONES******************/
+        public void ActualizarRegistro_Factores(string parametro, string valor, int cod) 
+        {
+            int opcion;
+            if (parametro == "Tabla")
+            {
+                opcion = 1;
+            }
+            else
+            {
+                if (parametro == "Concepto")
+                {
+                    opcion = 2;
+                }
+                else 
+                {
+                    if (parametro == "Monto")
+                    {
+                        opcion = 3;
+                    }
+                    else 
+                    {
+                        opcion = 4;
+                    }
+                }
+            }
+
+            switch(opcion)
+            {
+                case 1: dfactores.ModificarTabla(cod, valor);
+                    break;
+                case 2: dfactores.ModificarConcepto(cod, valor);
+                    break;
+                case 3: dfactores.ModificarMonto(cod, valor);
+                    break;
+                case 4: dfactores.EliminarFactores(cod, int.Parse(valor));
+                    break;
+            }
+        }
     }
 }
